a511e9e0548bfe2df67dadedaae0a6e7267c7bcc
muhammed yasser al-sayed <muhammedshehap97@gmail.com>
2023-09-11 15:27:00 +0300

    init

---
+++ hackerNews/hackerNews/hackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  hackerNewsApp.swift
+//  hackerNews
+//
+//  Created by muhammed shehap on 11/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct hackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
